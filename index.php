c8619b75d7c3bd30053653872ee1de2cf2726e2f
Sean Mackay <seannmackay@gmail.com>
2019-05-30 19:15:21 -0400

    Cleaned up and improved text entry fields and buttons as well as added drop down menu for classes (needs implementation).

--- index.php
+++ index.php
@@ -8,67 +8,30 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
 <body>
 
-<!-- Side Navigation -->
-<!---<nav class="w3-sidebar w3-bar-block w3-card w3-animate-left w3-center" style="display:none" id="mySidebar">
-  <h1 class="w3-xxxlarge w3-text-theme">Side Navigation</h1>
-  <button class="w3-bar-item w3-button" onclick="w3_close()">Close <i class="fa fa-remove"></i></button>
-  <a href="#" class="w3-bar-item w3-button">Link 1</a>
-  <a href="#" class="w3-bar-item w3-button">Link 2</a>
-  <a href="#" class="w3-bar-item w3-button">Link 3</a>
-  <a href="#" class="w3-bar-item w3-button">Link 4</a>
-</nav> -->
+
+<style>
+.grid-container {
+  display: grid;
+  grid-column-start: 1;
+  grid-column-end: 3;
+  grid-template-columns: auto auto auto;
+  background-color: #2196F3;
+  padding: 10px;
+}
+</style>
 
 <!-- Header -->
 <header class="w3-container w3-theme w3-padding" id="myHeader">
-  <i onclick="w3_open()" class="fa fa-bars w3-xlarge w3-button w3-theme"></i>
-  <div class="w3-center">
-  <h4>UBCSE Peer Evaluation </h4>
+  <div class="grid-container">
+    <div class="grid-item"> <img src="https://avatars2.githubusercontent.com/u/13955966?s=200&v=4"> </div>
+    <div class="grid-item"> <font color="black"> <h1> UBCSE Peer Evaluation </h1> </font> </div>
   </div>
 </header>
 
-
-
 <hr>
-<div class="w3-row-padding w3-center w3-padding">
-
-<form class="w3-container w3-card-4">
-  <h2>Please type in your UB email address! You'll then receive a verifaction code you can type in farther down the page.</h2>
-  <div class="w3-section">
-    <input class="w3-input" type="text" required>
-    <a class="w3-center w3-button w3-theme">Get Verification Code</a>
-  </div>
-</form>
-
-<hr>
-
-
-<form class="w3-container w3-card-4">
-  <h2>Enter your verification code below, then click submit to see the peer evaluation form.</h2>
-  <div class="w3-section">
-    <input class="w3-input" type="text" required>
-    <a class="w3-center w3-button w3-theme">Submit!</a>
-  </div>
-  </div>
-</form>
-
-
-
-<hr>
-<h2 class="w3-center">Buttons</h2>
-<div class="w3-center">
-  <br>
-  <a class="w3-button w3-theme">Button</a>
-
-
-  <br><br>
-  <a class="w3-button w3-circle w3-large w3-black"><i class="fa fa-plus"></i></a>
-  <a class="w3-button w3-circle w3-large w3-theme"><i class="fa fa-plus"></i></a>
-  <a class="w3-button w3-circle w3-large w3-card-4"><i class="fa fa-plus"></i></a>
-</div>
-<br>
 <div class="w3-center">
   <div class="w3-dropdown-hover">
-    <button class="w3-button w3-theme">Dropdown <i class="fa fa-caret-down"></i></button>
+    <button class="w3-button w3-theme">Select Your Class <i class="fa fa-caret-down"></i></button>
     <div class="w3-dropdown-content w3-bar-block w3-border">
       <a href="#" class="w3-bar-item w3-button">Link 1</a>
       <a href="#" class="w3-bar-item w3-button">Link 2</a>
@@ -77,116 +40,40 @@
   </div>
 </div>
 
-</div>
 
 <hr>
-<div class="w3-center">
-  <h2>Pagination</h2>
-  <!-- Pagination -->
-  <div class="w3-center w3-padding-32">
-    <div class="w3-bar">
-      <a href="#" class="w3-bar-item w3-button w3-hover-theme">«</a>
-      <a href="#" class="w3-bar-item w3-button w3-theme w3-hover-theme">1</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-theme">2</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-theme">3</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-theme">4</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-theme">5</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-theme">»</a>
-    </div>
+
+<div class="w3-row-padding w3-center w3-padding ">
+
+<form class="w3-container w3-card-4 w3-light-blue">
+  <h2>Please type in your UB email address! You'll then receive a verification code you can type in further down the page.</h2>
+  <div class="w3-section">
+    <input class="w3-input w3-light-grey" type="text" required>
+    <button class="w3-center w3-button w3-theme">Get Verification Code</button>
   </div>
+</form>
+
+<hr>
+
+
+<form class="w3-container w3-card-4 w3-light-blue">
+  <h2>Enter your verification code below, then click submit to see the peer evaluation form.</h2>
+  <div class="w3-section">
+    <input class="w3-input w3-light-grey" type="text" required>
+    <button class="w3-center w3-button w3-theme">Submit!</button>
+  </div>
+</form>
+
+
+
 </div>
-<br>
 
 <!-- Footer -->
 <footer class="w3-container w3-theme-dark w3-padding-16">
-  <h3>Footer</h3>
+  <h3>Acknowledgements</h3>
   <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-  <div style="position:relative;bottom:55px;" class="w3-tooltip w3-right">
-    <span class="w3-text w3-theme-light w3-padding">Go To Top</span> 
-    <a class="w3-text-white" href="#myHeader"><span class="w3-xlarge">
-    <i class="fa fa-chevron-circle-up"></i></span></a>
-  </div>
-  <p>Remember to check out our  <a href="w3css_references.asp" class="w3-btn w3-theme-light" target="_blank">W3.CSS Reference</a></p>
+  <p> <a  class=" w3-theme-light" target="_blank"></a></p>
 </footer>
-
-<!-- Script for Sidebar, Tabs, Accordions, Progress bars and slideshows -->
-<script>
-// Side navigation
-function w3_open() {
-  var x = document.getElementById("mySidebar");
-  x.style.width = "100%";
-  x.style.fontSize = "40px";
-  x.style.paddingTop = "10%";
-  x.style.display = "block";
-}
-function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
-}
-
-// Tabs
-function openCity(evt, cityName) {
-  var i;
-  var x = document.getElementsByClassName("city");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  var activebtn = document.getElementsByClassName("testbtn");
-  for (i = 0; i < x.length; i++) {
-    activebtn[i].className = activebtn[i].className.replace(" w3-dark-grey", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " w3-dark-grey";
-}
-
-var mybtn = document.getElementsByClassName("testbtn")[0];
-mybtn.click();
-
-// Accordions
-function myAccFunc(id) {
-  var x = document.getElementById(id);
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
-
-// Slideshows
-var slideIndex = 1;
-
-function plusDivs(n) {
-  slideIndex = slideIndex + n;
-  showDivs(slideIndex);
-}
-
-function showDivs(n) {
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
-
-showDivs(1);
-
-// Progress Bars
-function move() {
-  var elem = document.getElementById("myBar");
-  var width = 5;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width == 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + '%';
-      elem.innerHTML = width * 1  + '%';
-    }
-  }
-}
-</script>
 
 </body>
 </html>
