848c7087b56175bbe625c647ea2a49eff1039cec
Justin Henderson <jeh24@buffalo.edu>
2019-06-10 13:31:34 -0400

    bug fixes

--- accessCodePage.php
+++ accessCodePage.php
@@ -43,7 +43,11 @@ hr {
   </form>
 
   <?php
-}
+//error logging
+error_reporting(-1); // reports all errors
+ini_set("display_errors", "1"); // shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "~/php-error.log");
 ?>
   <hr>
 
