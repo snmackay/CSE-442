0b9e74b65d14620b4d7c9804d0fd74d852540b11
Sean Mackay <seannmackay@gmail.com>
2019-06-25 18:00:44 -0400

    Finised normalizing scores script #76

--- scoreNormalizer.py
+++ scoreNormalizer.py
@@ -1,9 +1,10 @@
 
 import mysql.connector
+import csv
 
 #connect to sql database, handle errors.
 def connect():
-    print ("beep")
+    print("Connecting")
     try:
 
         conn = mysql.connector.connect(
@@ -13,7 +14,7 @@ def connect():
             database="cse442_542_2019_summer_teame_db"
         )
         if conn.is_connected():
-            print("connection established.")
+            print("Connection to database established.")
         else:
             print("connection failed.")
         return conn
@@ -22,14 +23,25 @@ def connect():
     return conn
 
 
+def createCSV(inputDict):
+    with open("scores.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Name", "email", "Course", "Group Number", "Normalized Score"])
+        writer.writerows(inputDict)
+
+    print("Data exported to scores.csv")
+
 def process():
     mydb=connect()
     mycursor=mydb.cursor()
-    mycursor.execute("Select Name, course, group_number, recieved_scores FROM cse442")
+    mycursor.execute("Select Name, email, course, group_number, recieved_scores FROM cse442")
 
     myresult = mycursor.fetchall()
 
+    print("Data Gathered")
+
     rawArr=[]
+
     for x in myresult:
         rawStr=str(x)
         processedStr=rawStr.split(", ")
@@ -40,11 +52,49 @@ def process():
         elems[0]=elems[0][:-1]
         elems[1]=elems[1][1:]
         elems[1]=elems[1][:-1]
-        elems[3]=elems[3][1:]
-        elems[3]=elems[3][:-2]
-        for b in elems:
-            print(b)
-        print(" ")
+        elems[2]=elems[2][1:]
+        elems[2]=elems[2][:-1]
+        elems[4]=elems[4][1:]
+        elems[4]=elems[4][:-2]
+        #for b in elems:
+            #print(b)
+        #print(" ")
+    print("Data Processed")
+    ProcessedFull=[]
+    student = 0
+    for elems2 in rawArr:
+        Processed=[]
+        Processed.append(elems2[0])
+        Processed.append(elems2[1])
+        Processed.append(elems2[2])
+        Processed.append(elems2[3])
+
+        processedScore=elems2[4].split(":")
+
+        numMems=len(processedScore)
+        maxScore=numMems*15
+
+        justNums=[]
+        for b in processedScore:
+            temp=b.split(",")
+            del temp[0]
+            justNums=justNums+temp
+
+        justNumsInts=list(map(int,justNums))
+        totalEarned=sum(justNumsInts)
+
+        normalizedScore=totalEarned/maxScore
+
+        Processed.append(str(normalizedScore))
+        ProcessedFull.append(Processed)
+        student+=1;
+
+    createCSV(ProcessedFull)
+
+
+
+
+
 
 
 if "__name__==__main__": process()
